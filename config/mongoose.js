cf65e2cb7b21da57ef12bfa51a3bed108e8e500e
Kiran Veer <kiranveerabhadranavar847@gmail.com>
2023-06-18 15:39:55 +0530

    7. Border between category and duedate is added and dbURL is shifted to .env

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(URL);
+mongoose.connect(process.env.DBURL);
 const db = mongoose.connection;
 db.on('error', () => console.error.bind(console, 'Error connecting to database'));
 db.once('open', () => console.log('Connected to Database.....................................................................'));

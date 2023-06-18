c49698503dcebb075a4236d233aa8031f21dd25a
Kiran Veer <kiranveerabhadranavar847@gmail.com>
2023-06-18 14:43:48 +0530

    Commit after getting back the commit files of 5. Border between category and due date is added

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.DBURL);
+mongoose.connect(URL);
 const db = mongoose.connection;
 db.on('error', () => console.error.bind(console, 'Error connecting to database'));
 db.once('open', () => console.log('Connected to Database.....................................................................'));
